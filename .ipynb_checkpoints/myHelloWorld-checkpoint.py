3c20adae69c1b3ab63b438241ae1eb2f715b9529
Evangelos Tzimopoulos <e.tzimopoulos@gmail.com>
2020-09-21 06:50:28 +0100

    Hello world for streamlit article

---
+++ .ipynb_checkpoints/myHelloWorld-checkpoint.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# My first Streamlit App\n",
+    "st.title('Hello Streamlit World')\n",
+    "st.header('Introduction')\n",
+    "st.write(\"Hello World, this is my first Web App built using Streamlit\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
---
+++ myHelloWorld.py
@@ -0,0 +1,5 @@
+import streamlit as st
+# My first Streamlit App"
+st.title('Hello Streamlit World')
+st.header('Introduction')
+st.write("Hello World, this is my first Web App built using Streamlit.")
